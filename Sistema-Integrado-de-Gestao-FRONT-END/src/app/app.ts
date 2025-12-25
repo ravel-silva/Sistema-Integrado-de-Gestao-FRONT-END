c5293e23b78087ad9e81adedec17ddd7d43ad1f3
Ravel Mesquita <ravels78@gmail.com>
2025-12-24 22:56:08 -0300

    feat: criar componente equipes

--- Sistema-Integrado-de-Gestao-FRONT-END/src/app/app.ts
+++ Sistema-Integrado-de-Gestao-FRONT-END/src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Home } from '../components/home/home';
+import { Equipes } from "../components/equipes/equipes";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home],
+  imports: [RouterOutlet, Home, Equipes],
+  standalone: true,
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ Sistema-Integrado-de-Gestao-FRONT-END/src/components/equipes/equipes.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-equipes',
+  imports: [],
+  standalone: true,
+  templateUrl: './equipes.html',
+  styleUrl: './equipes.css',
+})
+export class Equipes {
+
+}
